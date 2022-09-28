623787b7036e3039fbd36f52e576b6fad907d3df
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-28 19:41:33 +0000

    added more notes using map functionb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import Header from "./Header";
 import Note from './Note';
 import Footer from "./Footer";
+import notes from "../notes";
+
+function noteCreate(note){
+    return(
+    <Note
+    key = {note.key}
+    title = {note.title}
+    content = {note.content}
+     />
+    );
+}
+
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map(noteCreate)}
             <Footer />
         </div>
     );
